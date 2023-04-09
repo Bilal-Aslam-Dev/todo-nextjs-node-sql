afd15bac926bb1cbc2bc17feda5eee1eb89d936a
bilal1999-cyber <103093008+bilal1999-cyber@users.noreply.github.com>
2023-04-09 14:07:32 +0500

    checking

--- src/components/screens/home/HomeScreen.tsx
+++ src/components/screens/home/HomeScreen.tsx
@@ -1,10 +1,11 @@
 import type { FC } from 'react'
 
 const HomeScreen: FC = () => {
+  const abc;
   return (
     <>
       <div className="home-gradient h-screen w-screen flex flex-col justify-center items-center">
-        
+
       </div>
     </>
   )
